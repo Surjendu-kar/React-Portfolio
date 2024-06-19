67ed5e3006d115e17196809834d2b96c880221b7
Surjendu kar <rahulgotp9988@gmail.com>
2024-06-19 13:31:24 +0530

    add Custom Cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,32 @@
-import React from 'react'; // Add this line to import React
-
-// Import CSS in order
-import 'normalize.css';
-// import './animate.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import './img/icons/css/ionicons.css';
-import './img/font-awesome/css/font-awesome.css';
-import 'lightbox2/dist/css/lightbox.min.css'
-import './App.css';
-
-// Import JS libraries
-import 'jquery/dist/jquery.min.js';
-// import 'popper.js/dist/popper.min.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './libs/easing.js';
-import 'lightbox2/dist/js/lightbox.min.js';
-
-// Import components
-import Navbar from './components/Navbar.js';
-import About from './components/About.js';
-import BackToTop from './components/BackToTop.js';
-import Contact from './components/Contact.js';
-import Intro from './components/Intro.js';
-// import Preloader from './components/Preloader.js';
-import Projects from './components/Projects.js';
+import React from "react"; 
+import "normalize.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./img/icons/css/ionicons.css";
+import "./img/font-awesome/css/font-awesome.css";
+import "lightbox2/dist/css/lightbox.min.css";
+import "./App.css";
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./libs/easing.js";
+import "lightbox2/dist/js/lightbox.min.js";
+import Navbar from "./components/Navbar.js";
+import About from "./components/About.js";
+import BackToTop from "./components/BackToTop.js";
+import Contact from "./components/Contact.js";
+import Intro from "./components/Intro.js";
+import Cursor from "./components/Cursor";
+import Projects from "./components/Projects.js";
 
 function App() {
   return (
     <React.Fragment>
-        <Navbar />
-        <Intro />
-        <About />
-        <Projects />
-        <Contact />
-        <BackToTop />
-        {/* <Preloader /> */}
+      <Cursor />
+      <Navbar />
+      <Intro />
+      <About />
+      <Projects />
+      <Contact />
+      <BackToTop />
     </React.Fragment>
   );
 }
