77ea48df4b892033405eccda094fee1e005f5b9f
Surjendu kar <rahulgotp9988@gmail.com>
2024-07-20 22:13:08 +0530

    Fix the Style in glow.css

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -2,19 +2,17 @@ import { useEffect, useRef } from "react";
 import "../libs/easing.js";
 
 const BackToTop = () => {
-  // Specify the type for useRef
   const backToTopRef = useRef<HTMLAnchorElement>(null);
 
   useEffect(() => {
     const handleScroll = () => {
-      // Check if the current reference is not null
       if (backToTopRef.current) {
         if (window.pageYOffset > 150) {
           backToTopRef.current.classList.remove("fadeOut");
           backToTopRef.current.style.display = "block";
           backToTopRef.current.classList.add("fadeIn");
         } else {
-          backToTopRef.current.style.display = "none"; // Hide when not scrolled down
+          backToTopRef.current.style.display = "none";
         }
       }
     };
