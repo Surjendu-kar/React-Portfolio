a5d03a7a0ad2ba48a6edb8fd24df6dc2e3ce4326
Surjendu kar <rahulgotp9988@gmai.com>
2025-07-31 13:57:13 +0530

    refactor(Intro): remove ShinyText component to streamline the Intro section and improve code clarity

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
-import ShinyText from "./ui/ShinyText";
 
 const Heading = styled(Typography)(({ theme }) => ({
   // color: "#fff",
@@ -160,11 +159,6 @@ const Intro = () => {
                   alignItems: "center",
                 }}
               >
-                <ShinyText
-                  text="Just some shiny text!"
-                  disabled={false}
-                  speed={3}
-                />
                 <ButtonStyle
                   variant="contained"
                   href="https://drive.google.com/file/d/1NngFzR46TYGa3megPc2EGctynUVxyEX9"
